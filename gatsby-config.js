b52d712e41f7461c129d92c88a2a0e182606e3c5
Steven Riady <riadysteve@gmail.com>
2020-09-12 20:58:54 +0700

    Commit 3: Data in Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,16 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
   plugins: [
+    `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
-      }
-    }
+      },
+    },
   ],
 }
